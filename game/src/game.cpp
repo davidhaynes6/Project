5054bad6ea9a65d84b9e9f5877940954fec76704
David Haynes <david.m.haynes@gmail.com>
2023-01-02 13:56:58 -0500

    Flappy example

--- game/src/game.cpp
+++ game/src/game.cpp
@@ -2,9 +2,10 @@
 
 project::app_config project::create_application() {
     project::app_config config;
+    config.scene = "assets/scene.yaml";
     config.version = "0.0.1";
-    config.title = "Project2D";
-    config.height = 1080;
-    config.width = 720;
+    config.title = "Flappy Plane";
+    config.height = 720;
+    config.width = 320;
     return config;
 }
